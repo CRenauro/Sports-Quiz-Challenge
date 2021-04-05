f1d5ee53bbadeb6aa7e991c3904d1f57a84b8356
CRenauro <crenauro@optimum.net>
2021-04-05 19:21:37 -0400

    fixed typos

--- assets/script.js
+++ assets/script.js
@@ -6,93 +6,62 @@ var scoreEl = document.querySelector("#score");
 
 //sections and intro//
 
-var introEl = document.querySelector("#intro");
-var questionsEl = document.querySelector("#questions");
+const introEl = document.querySelector("#intro");
+const questionsEl = document.querySelector("#questions");
 var questionEl = document.querySelector("#question");
 var questionCount = 0;
-var rightorwrongEl = document.querySelector("#rightorwrong");
+const rightorwrongEl = document.querySelector("#rightorwrong");
 
 //final//
-var finaLEl = document.querySelector("#final");
+const finalEl = document.querySelector("#final");
 var enterInitials = document.querySelector("#initials");
 
 
 //scores//
-var highscoresEl = document.querySelector("#highscores");
+const highscoresEl = document.querySelector("#highscores");
 var scoreListEl = document.querySelector("#score-list");
 var scoreList = [];
 
 //buttons//
-var startBtn = document.querySelector("#start");
-var ansBtn = document.querySelectorAll("button.ansBtn")
-var ansBtn1 = document.querySelector("#answer1");
-var ansBtn2 = document.querySelector("#answer2");
-var ansBtn3 = document.querySelector("#answer3");
-var ansBtn4 = document.querySelector("#answer4");
-var submitScrBtn = document.querySelector("#submit-score");
-var returnBtn = document.querySelector("#return");
-var clearScrBtn = document.querySelector("#clearscores");
-var viewScrBtn = document.querySelector("#view-scores");
+const startBtn = document.querySelector("#start");
+const ansBtn = document.querySelectorAll("button.ansBtn")
+const ansBtn1 = document.querySelector("#answer1");
+const ansBtn2 = document.querySelector("#answer2");
+const ansBtn3 = document.querySelector("#answer3");
+const ansBtn4 = document.querySelector("#answer4");
+const submitScrBtn = document.querySelector("#submit-score");
+const returnBtn = document.querySelector("#return");
+const clearScrBtn = document.querySelector("#clearscores");
+const viewScrBtn = document.querySelector("#view-scores");
 
 
-var questions = [
-  {  
-    question: "In what state is the Churchill Downs horse track located?",
-    answers: {
-        a:"Ohio", 
-        b:"Kentucky", 
-        c:"Virginia", 
-        d:"Pennsylvania"
+const questions = [
+    {  
+        question: "In what state is the Churchill Downs horse track located?",
+        answers: ["1. Ohio", "2. Kentucky", "3. Virginia", "4. Pennsylvania"],
+        correctAnswer: "2"         
     },
-    correctAnswer: "b"         
-},
-
-{     
-    question: "What was the original Yankee stadium called?",
-    answers: {
-        a:"Heritage Field",
-        b:"Polo Grounds",
-        c:"Meadowlands",
-        d:"Gotham Field" 
+    {     
+        question: "What was the original Yankee stadium called?",
+        answers: ["1. Heritage Field", "2. Polo Grounds", "3. Meadowlands", "4. Gotham Field"],
+        correctAnswer: "1" 
     },
-    correctAnswer: "a" 
-},
-
-{  
-    question: "What state did the baseball franchise, the San Franciso Giants, move from?",
-    answers: {
-        a:"Nevada",
-        b:"New York",
-        c:"New Jersey",
-        d:"Connecticut"
+    {  
+        question: "What state did the baseball franchise, the San Franciso Giants, move from?",
+        answers: ["1. Nevada", "2. New York", "3. New Jersey", "4. Connecticut"],
+        correctAnswer: "2" 
     },
-    correctAnswer: "b" 
-},
-
-{  
-    question: "In what year was the NY Giants football team established?",
-    answers: {
-        a:"1920",
-        b:"1960",
-        c:"1925",
-        d:"1953"
-    },
-    correctAnswer: "c"
+    {  
+        question: "In what year was the NY Giants football team established?",
+        answers: ["1. 1920", "2. 1960", "3. 1925", "4. 1953"],
+        correctAnswer: "3"
     
-},
-
-{  
-    question: "How many Stanley Cups have the New Jersey Devils hockey team won?",
-    answers: {
-        a:"0",
-        b:"5",
-        c:"2",
-        d:"3", 
     },
-    correctAnswer: "d"
-        
-  }
-
+    {  
+        question: "How many Stanley Cups have the New Jersey Devils hockey team won?",
+        answers: ["1. 0", "2. 5", "3. 2", "4. 3"],
+        correctAnswer: "3"    
+    }
 ];
 
 
@@ -105,10 +74,10 @@ function setTime() {
         if (secondsLeft === 0 || questionCount === questions.length) {
             clearInterval(timerInterval);
             questionsEl.style.display = "none";
-            finaLEl.style.display = "block";
+            finalEl.style.display = "block";
             scoreEl.textContent = secondsLeft;
         }
-    }, 1000);
+    }, 2000);
 }
 
 function startQuiz() {
@@ -121,32 +90,6 @@ function startQuiz() {
 }
 
 
-
-
-
-function getQuestion() {
-    var currentQuestion = questions[currentQuestionIndex];
-    var titleEl = document.getElementById(questions);
-    titleEl.textContent = currentQuestionIndex.title;
-    choicesEl.innerHTML = "";
-    currentQuestionIndex.answers.forEach(function(choice, i) {
-        var choiceNode = document.createElement("button");
-        choiceNode.setAttribute("class", "choice");
-        choiceNode.setAttribute("value", choice);
-        choiceNode.textContent = i + 1 + ". " + choice;
-        choiceNode.onclick = questionClick;
-        choicesEl.appendChild(choiceNode);
-    });
-}
-
-function startQuiz() {
-  titleScreen.setAttributes('class', 'hide');
-  quizScreen.setAttributes('class', 'show');
-  timerId = setInterval(tick, 1000);
-  timeEl.textContent = time;
-  getQuestion();
-}
-
 function setQuestion(id) {
     if (id < questions.length) {
         questionEl.textContent = questions[id].question;
@@ -155,7 +98,6 @@ function setQuestion(id) {
         ansBtn3.textContent = questions[id].answers[2];
         ansBtn4.textContent = questions[id].answers[3];
     }
-
 }
 
 function checkAnswer(event) {
@@ -187,11 +129,11 @@ function checkAnswer(event) {
 function addScore(event) {
     event.preventDefault();
 
-    finaLEl.stle.display = "none";
+    finalEl.style.display = "none";
     highscoresEl.style.display = "block";
 
     var init = enterInitials.value.toUpperCase();
-    scoreList.push({ initals. init, score: secondsLeft });
+    scoreList.push({ initals, init, score: secondsLeft });
 
     scoreList = scoreList.sort((a, b) => {
         if (a.score < b.score) {
@@ -217,3 +159,43 @@ function addScore(event) {
 function storeScores() {
     localStorage.setItem("scoreList", JSON.stringify(scoreList));
 }
+
+
+function displayScores() {
+    var storedScore = JSON.parse(localStorage.getItem("scoreList"));
+
+    if (storedScore !== null) {
+        scoreList = storedScore;
+    }
+}
+
+function clearScores() {
+    localStorage.clear();
+    scoreListEl,innerHTML="";
+
+}
+
+startBtn.addEventListener("click", startQuiz);
+ansBtn.forEach(item => {
+    item.addEventListener('click', checkAnswer);
+});
+
+submitScrBtn.addEventListener("click", addScore);
+returnBtn.addEventListener("click", function () {
+    highscoresEl.style.display = "none";
+    introEl.style.display = "block";
+    secondsLeft = 60;
+    timeEl.textContent = `Time:${secondsLeft}s`;
+});
+
+clearScrBtn.addEventListener("click", clearScores);
+
+viewScrBtn.addEventListener("click", function () {
+    if (highscoresEl.style.display === "none") {
+        highscoresEl.style.display = "block";
+    } else if (highscoresEl.style.display === "block") {
+        highscoresEl.style.display = "none";
+    } else {
+        return alert("no scores to show");
+    }
+});
